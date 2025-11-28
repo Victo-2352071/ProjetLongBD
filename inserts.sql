2f8887e3436ba3cc2ffbfd50c4a9a8549749f8d9
Victo-2352071 <maikm2006@hotmail.com>
2025-11-28 10:46:48 -0500

    innitial commit mais ya deja du stock vu que jutilise pas git comme il faut

---
+++ inserts.sql
@@ -0,0 +1,24 @@
+USE database_pizza;
+
+INSERT INTO croutes (nom) VALUES
+('Classique'),
+('Mince'),
+('Epaisse');
+
+INSERT INTO sauces (nom) VALUES
+('Tomate'),
+('Spaghetti'),
+('Alfredo');
+
+INSERT INTO garnitures (nom) VALUES
+('Pepperoni'),
+('Champignons'),
+('Oignons'),
+('Poivrons'),
+('Olives'),
+('Anchois'),
+('Bacon'),
+('Poulet'),
+('Maïs'),
+('Fromage'),
+('Piments forts');
---
+++ website/app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template
+import mysql.connector
+
+app = Flask(__name__)
+
+# Configuration de la connexion à la base de données
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="mysql",
+    database="database_pizza"
+)
+
+@app.route('/')
+def index():
+    cursor = db.cursor()
+    cursor.execute("SELECT *")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
